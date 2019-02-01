d43184e91b66f0b8f33ee3bb1efc7025a8972103
Stewart <stewartwadehines@gmail.com>
2019-02-01 07:20:31 -0600

    Added a layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './common/Button';
-import Card from './common/Card';
 import {ThemeProvider} from 'styled-components';
 import WireTheme from './theme/wire';
+import Button from './common/Button';
+import Card from './common/Card';
+import Layout from './common/Layout';
+
+
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={WireTheme}>
-      <Card>
-          <Button>
-              Base button
-          </Button>
-          <Button primary>
-              Primary button
-          </Button>
-          <Button secondary>
-              Secondary Button
-          </Button>
-        </Card>
+      <Layout>
+        <Card>
+            <Button>
+                Base button
+            </Button>
+            <Button primary>
+                Primary button
+            </Button>
+            <Button secondary>
+                Secondary Button
+            </Button>
+          </Card>        
+        </Layout>
       </ThemeProvider>
     );
   }
--- src/theme/wire.js
+++ src/theme/wire.js
@@ -1,12 +1,24 @@
 const WireTheme = {
+  // Colors
   white: 'white',
   lightBlue: '#f1f9ff',
   midBlue: '#bce0fd',
   blue: '#7fc4fd',
   darkBlue: '#2699fb',
 
-  borderRadiusGlobal: '8px'
+  // Border
+  borderRadiusGlobal: '8px',
   
+  // Layout
+  layoutMaxWidth: '1024px',
+
+  // Type
+  layoutFontFamily: "'Helvetica', Arial, sans-serif",
+  layoutFontSize: '1rem',
+  layoutFontWeight: '300',
+  layoutLineHeight: '1.5rem',
+  
+
 };
 
 export default WireTheme;
